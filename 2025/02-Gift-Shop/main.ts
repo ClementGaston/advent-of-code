809e81efccf1b2b6214d5e64a6a08001a59e3d86
Clément GASTON <clement.gaston@artal.fr>
2025-12-04 13:01:40 +0100

    02 - Gift Shop - Part 02

--- 2025/02-Gift-Shop/main.ts
+++ 2025/02-Gift-Shop/main.ts
@@ -4,6 +4,7 @@ import { getStringInput } from "../../_helpers/readInput";
 const inputs = getStringInput(__dirname).split(",");
 
 let sumOfInvalidId = 0;
+let sumOfInvalidIdPartTwo = 0;
 
 for (const range of inputs) {
 	// Parse row input
@@ -21,18 +22,47 @@ for (const range of inputs) {
 		throw new Error("Invalid rotation format");
 	}
 
-	for (let i = nbrStartRange; i <= nbrEndRange; i++) {
-		const stringifyNumber = i.toString();
+	// First Part
+	for (let id = nbrStartRange; id <= nbrEndRange; id++) {
+		const stringifyNumber = id.toString();
 
 		// Pass all odds number as they can't have repeated patterns
+		const halfLength = stringifyNumber.length / 2;
 		if (stringifyNumber.length % 2 !== 0) continue;
 
-		const halfLength = stringifyNumber.length / 2;
 		const firstHalf = stringifyNumber.slice(0, halfLength);
 		const secondHalf = stringifyNumber.slice(halfLength);
 
-		if (firstHalf === secondHalf) sumOfInvalidId += i;
+		if (firstHalf === secondHalf) sumOfInvalidId += id;
+	}
+
+	// Second Part
+	for (let id = nbrStartRange; id <= nbrEndRange; id++) {
+		const stringifyNumber = id.toString();
+
+		const length = stringifyNumber.length;
+		const halfLength = Math.floor(length / 2);
+
+		let isIdFlagged = false;
+
+		for (let i = 1; i <= halfLength; i++) {
+			if (length % i !== 0 || isIdFlagged) continue;
+
+			let nbOfRepeatPossiblePattern = length / i;
+			let patternSize = length / nbOfRepeatPossiblePattern;
+			let presentNumbers = new Set();
+
+			for (let j = 0; j < length; j += patternSize) {
+				presentNumbers.add(stringifyNumber.slice(j, j + patternSize));
+			}
+
+			if (presentNumbers.size === 1) {
+				sumOfInvalidIdPartTwo += id;
+				isIdFlagged = true;
+			}
+		}
 	}
 }
 
-printSolution("The sums of all invalid IDS is $$", [sumOfInvalidId]);
+printSolution("The sums of a subset of invalid IDS is $$", [sumOfInvalidId]);
+printSolution("The sums of all invalid IDS is $$", [sumOfInvalidIdPartTwo]);
